c835e873eabab0b2b8d46c516a7e118fa2379447
Felipe Rios <felipe.rios.silva@outlook.com>
2021-08-16 13:31:31 -0300

    Initial commit
    Signed-off-by: Felipe Rios <felipe.rios.silva@outlook.com>

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      QueueHelper.SendMessage("test", "message1");
+
+      Console.WriteLine(Guid.NewGuid().ToString());
+      QueueHelper.SendMessage("test", "message2");
+
+      Console.WriteLine(Guid.NewGuid().ToString());
+      QueueHelper.SendMessage("test", "message3");
+
+      Console.WriteLine(Guid.NewGuid().ToString());
+      QueueHelper.SendMessage("test", "message4");
+
+      Console.WriteLine(Guid.NewGuid().ToString());
+      QueueHelper.SendMessage("test", "message5");
+    }
+  }
+}
---
+++ QueueHelper.cs
@@ -0,0 +1,33 @@
+﻿﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Confluent.Kafka;
+
+ namespace ConsoleApp1
+{
+  public static class QueueHelper
+  {
+    private const string QueueAddressesList = "kafka:9200";
+
+    public static void SendMessage(string topic, string message)
+    {
+      LoadNativeKafkaLib();
+      var producer = new ProducerBuilder<Null, string>(new ProducerConfig
+      {
+        ClientId = Dns.GetHostName(),
+        BootstrapServers = QueueAddressesList
+      }).Build();
+      producer.Produce(topic, new Message<Null, string> {Value = message});
+    }
+
+    private static void LoadNativeKafkaLib()
+    {
+      //var tmpDirectory = new DirectoryInfo(Path.Combine(Path.GetTempPath(), "Costura"));
+      //var costuraPath = Path.Combine(tmpDirectory
+      //  .GetDirectories().First().FullName, Environment.Is64BitProcess ? "64" : "32");
+      //Library.Load(Path.Combine(costuraPath, "librdkafka.dll"));
+      Library.Load("librdkafka.dll");
+    }
+  }
+}
